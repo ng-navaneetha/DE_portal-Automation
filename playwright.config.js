69461a8fba6800644c204a3026419469fc52cc07
ng-navaneetha <navaneetha.b@ngenux.com>
2025-10-31 12:02:35 +0530

    updated the reporter file path

--- playwright.config.js
+++ playwright.config.js
@@ -1,44 +1,39 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * @see https://playwright.dev/docs/test-configuration
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
-
   /* Run tests in files in parallel */
   fullyParallel: true,
-
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
-  /* Reporter to use. */
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'], // shows readable output in Actions logs
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ['json', { outputFile: 'results.json' }]
+    ['html'],
+    ['json', { outputFile: 'results.json' }],
+    
   ],
-
-  /* Test coverage and reporting */
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL (optional) */
+    /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test */
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
-    /* Enable video recording for failed tests */
-    video: 'retain-on-failure',
-
-    /* Take screenshot on failure */
-    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -48,14 +43,34 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // Uncomment if needed
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
+
     // {
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
 
